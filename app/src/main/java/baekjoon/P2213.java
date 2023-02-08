a017a419f89d9041f0c8d4c4abda85c568a43629
MinGyu2 <mq04034@naver.com>
2023-02-08 14:10:06 +0900

    문제 품

--- app/src/main/java/baekjoon/P2213.java
+++ app/src/main/java/baekjoon/P2213.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class P2213 {
@@ -31,95 +30,69 @@ public class P2213 {
         }
         boolean[] visited = new boolean[n+1];
         // 입력 끝
+        var ans = independentGetSize(tree, visited, weight, 1);
+        var sb = new StringBuilder();
+        if(ans.a > ans.b){
+            sb.append(ans.a).append('\n');
+            ans.listA.sort((a,b)->a-b);
+            for(int v:ans.listA){
+                sb.append(v).append(' ');
+            }
+        }else{
+            sb.append(ans.b).append('\n');
+            ans.listB.sort((a,b)->a-b);
+            for(int v:ans.listB){
+                sb.append(v).append(' ');
+            }
+        }
+        System.out.println(sb);
+
         // System.out.println(tree);
         // System.out.println(Arrays.toString(weight));
-        System.out.println(independentSetSize(tree, visited, weight, 7));
-
-        // System.out.println();
-        // var nlist = new NodeList();
-        // nlist.add(1);
-        // // nlist.add(2);
-        // // nlist.add(3);
-
-        // var nlist2 = new NodeList();
-        // nlist2.add(4);
-        // nlist2.add(5);
-        // nlist2.add(6);
-        // nlist.merge(nlist2);
-
-        // System.out.println(nlist);
-        // System.out.println(nlist2);
+        System.out.println(ans);
     }
-    static Pair independentSetSize(ArrayList<ArrayList<Integer>> tree, boolean[] visited, int[] weight, int root){
+    static Pair independentGetSize(ArrayList<ArrayList<Integer>> tree, boolean[] visited, int[] weight, int root){
         visited[root] = true;
         int a = 0;
         int b = weight[root];
+
+        var listA = new ArrayList<Integer>();
+        var listB = new ArrayList<Integer>();
         for(int u:tree.get(root)){
             if(visited[u]){
                 continue;
             }
-            var temp = independentSetSize(tree, visited, weight,u);
+            var temp = independentGetSize(tree, visited, weight,u);
+            listB.addAll(temp.listB);
             b += temp.b;
-            a += temp.getMax();
+            
+            if(temp.a > temp.b){
+                a += temp.a;
+                listA.addAll(temp.listA);
+            }else{
+                a += temp.b;
+                listA.addAll(temp.listB);
+            }
         }
-        return new Pair(b,a);
+        listB.add(root);
+        return new Pair(b,a, listB, listA);
     }
     static class Pair{
         final int a,b;
-        public Pair(int a,int b){
+        final ArrayList<Integer> listA;
+        final ArrayList<Integer> listB;
+        public Pair(int a,int b, ArrayList<Integer> listA, ArrayList<Integer> listB){
             this.a = a;
             this.b = b;
+            this.listA =listA;
+            this.listB = listB;
         }
-        public int getMax(){
-            return a > b ? a : b;
-        }
+        // public int getMax(){
+        //     return a > b ? a : b;
+        // }
         @Override
         public String toString() {
-            return String.format("(%d %d)",a,b);
-        }
-    }
-    static class Node{
-        private Node next;
-        private int value;
-        public Node(int value, Node next){
-            this.value = value;
-            this.next = next;
-        }
-        public void setNext(Node next) {
-            this.next = next;
-        }
-        @Override
-        public String toString() {
-            return String.format("%d %s",value,next);
-        }
-    }
-    static class NodeList{
-        Node first = null;
-        Node end = null;
-        public NodeList(){}
-        public void add(int v){
-            Node n = new Node(v, null);
-            if(end != null){
-                end.setNext(n);
-            }
-            end = n;
-            if(first == null){
-                first = n;
-            }
-        }
-        public void merge(NodeList nodeList){
-            if(this.end != null){
-                this.end.next = nodeList.first;
-            }
-            this.end = nodeList.end;
-
-            if(this.first == null){
-                this.first = nodeList.first;
-            }
-        }
-        @Override
-        public String toString() {
-            return String.format("(%s)",first);
+            return String.format("(%d %d %s %s)",a,b,listA, listB);
         }
     }
 }
